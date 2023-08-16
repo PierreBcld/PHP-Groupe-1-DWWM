f34d54703e88c2b32f5a0e3bb9f07b34c912df51
Pierre.Bcld <pierre.bcld@gmail.com>
2023-08-16 11:23:51 +0200

    Main

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
 include './gestion_eleve/eleve.php';
+
+$nom = readline("Veuillez entrer le nom de l'élève : ");
+$i = 1;
+
+$nombreDeNotes = readline("Combien de notes : ");
+$listNote = [];
+while ($i <= $nombreDeNotes) {
+    $note = readline("Veuillez insérer la note numéro " . $i . " : ");
+    array_push($listNote, $note);
+    $i++;
+}
+
+$moyenne = array_sum($listNote)/$nombreDeNotes;
+echo ($nom . " a une moyenne de " . $moyenne . " !");
